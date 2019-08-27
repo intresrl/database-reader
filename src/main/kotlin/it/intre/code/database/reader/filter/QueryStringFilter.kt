b77e178ff5fe92ccaf1537fcfb32a6a419e9a68e
Alessandro D'Amico <damico_alex@hotmail.com>
2019-08-27 17:27:29 +0200

    Added queryStringFilter in kt

---
+++ src/main/kotlin/it/intre/code/database/reader/filter/QueryStringFilter.kt
@@ -0,0 +1,68 @@
+package it.intre.code.database.reader.filter
+
+import it.intre.code.database.reader.config.Column
+import kotlin.math.max
+
+/**
+ * Container for all standard query options, taken from query string (e.g."?page=3&size=50")
+ */
+data class QueryStringFilter internal constructor(
+        /**
+         * If not blank, return only the last value of field with this name and the number of rows.
+         */
+        var last: String?,
+        /**
+         * Page number (starting from 0)
+         */
+        var page: Int?,
+        /* The following two parameters (pageFrom and pageTo) are used in alternative to "page" parameter when getting more than one page */
+        /**
+         * First page number
+         */
+        var pageFrom: Int?,
+        /**
+         * Last page number
+         */
+        var pageTo: Int?,
+        /**
+         * Page size
+         */
+        var size: Int?,
+        /**
+         * Optional list of fields to SELECT (if empty, all fields in profile will be returned)
+         */
+        var reportFields: List<String> = listOf(),
+        /**
+         * Optional list of fields to use in GROUP BY clause
+         */
+        var groupBy: List<String> = listOf(),
+        /**
+         * List of fields to use in ORDER BY clause.
+         * Works in pair with [.orderDir] to produce [.getOrderFields].
+         */
+        // FIXME DAMIANO parallel lists... replace with map?
+        private var orderBy: List<String>?,
+        /**
+         * List of ordering directions in ORDER BY clause.
+         * Works in pair with [.orderBy] to produce [.getOrderFields].
+         */
+        private var orderDir: List<String>?) {
+
+
+    val orderFields: List<OrderField>
+        get() {
+            val fieldNames = orderBy ?: listOf()
+            val fieldDirs = (orderDir ?: listOf())
+            val take = generateSequence { OrderField.ASC }.take(max(0,fieldNames.size - fieldDirs.size))
+            val dirs = (fieldDirs + take).slice(0 until fieldNames.size)
+
+            return fieldNames.zip(dirs).map { (fieldId,dir) -> OrderField(fieldId,dir) }
+        }
+
+    fun getGroupAsString(toRemove: String) = groupBy.filter { !it.equals(toRemove, true)}.joinToString(",")
+
+
+    fun isContainedInGroup(column: Column) = groupBy.any{ x -> x.equals(column.name, true)}
+
+    fun hasGroupBy() = groupBy.isNotEmpty()
+}
