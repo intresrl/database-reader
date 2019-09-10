14f0b80ebec85f7f85b42c94de17b335cd9cb874
Alessandro D'Amico <damico_alex@hotmail.com>
2019-09-10 17:07:17 +0200

    Add test into QueryStringFilterTest

--- src/test/kotlin/it/intre/code/database/reader/filter/QueryStringFilterTest.kt
+++ src/test/kotlin/it/intre/code/database/reader/filter/QueryStringFilterTest.kt
@@ -1,17 +1,21 @@
 package it.intre.code.database.reader.filter
 
+import it.intre.code.database.reader.TestArguments
 import it.intre.code.database.reader.config.Column
 import it.intre.code.database.reader.sql.SqlMatcher
 import org.hamcrest.MatcherAssert.assertThat
-import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import java.util.Arrays.asList
+import java.util.stream.Stream
 
 class QueryStringFilterTest {
 
-    private var filter: QueryStringFilter  = QueryStringFilter()
+    private var filter: QueryStringFilter = QueryStringFilter()
 
     @BeforeEach
     fun setUp() {
@@ -29,7 +33,7 @@ class QueryStringFilterTest {
     }
 
     private fun createColumn(name: String, alias: String = ""): Column {
-        return Column().apply { this.name=name; this.alias = alias}
+        return Column().apply { this.name = name; this.alias = alias }
     }
 
     @Test
@@ -42,5 +46,32 @@ class QueryStringFilterTest {
     fun getGroupAsStringEmpty() {
         filter.groupBy = emptyList()
         assertThat(filter.getGroupAsString(null), SqlMatcher.matchesSql(""))
+    }
+
+    companion object {
+        @JvmStatic
+        fun getArguments(): Stream<Arguments> = listOf<Arguments>(
+                TestArguments.of(listOf<OrderField>(), listOf<String>(), listOf<String>()),
+                TestArguments.of(listOf(
+                        OrderField("A", OrderField.ASC),
+                        OrderField("B", OrderField.DESC),
+                        OrderField("C", OrderField.ASC)
+                ), listOf("A", "B", "C"), listOf(OrderField.ASC, OrderField.DESC, OrderField.ASC)),
+                TestArguments.of(listOf(
+                        OrderField("A", OrderField.ASC)
+                ), listOf("A"), listOf(OrderField.ASC, OrderField.DESC, OrderField.ASC)),
+                TestArguments.of(listOf(
+                        OrderField("A", OrderField.ASC),
+                        OrderField("B", OrderField.ASC),
+                        OrderField("C", OrderField.ASC)
+                ), listOf("A", "B", "C"), listOf(OrderField.ASC))
+        ).stream()
+    }
+
+    @ParameterizedTest(name = "{1}+{2}->{0}")
+    @MethodSource("getArguments")
+    fun `Order field joins name and direction`(expected: List<OrderField>, orderBy: List<String>, orderDir: List<String>) {
+        val queryStringFilter = QueryStringFilter(orderBy = orderBy, orderDir = orderDir)
+        assertEquals(expected, queryStringFilter.orderFields)
     }
 }
