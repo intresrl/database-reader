c70c763ca092678a38f313edf9699b5132926a4d
Alessandro D'Amico <damico_alex@hotmail.com>
2019-09-10 16:38:05 +0200

    Add test into OrderFieldTest

--- src/test/kotlin/it/intre/code/database/reader/filter/OrderFieldTest.kt
+++ src/test/kotlin/it/intre/code/database/reader/filter/OrderFieldTest.kt
@@ -1,17 +1,27 @@
 package it.intre.code.database.reader.filter
 
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.Test
+import it.intre.code.database.reader.TestArguments
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 internal class OrderFieldTest {
 
-    @Test
-    fun `toString concatenates id and direction`() {
-        var cacca : CharSequence? = "CIAO"
-        println(cacca.isNullOrEmpty())
-        cacca=""
-        println(cacca.isNullOrEmpty())
-        cacca=null
-        println((cacca as CharSequence?).isNullOrEmpty())
+    companion object {
+        @JvmStatic
+        fun getArguments(): Stream<Arguments> = listOf<Arguments>(
+                TestArguments.of(" ", OrderField()),
+                TestArguments.of("id ", OrderField("id")),
+                TestArguments.of("id order", OrderField("id","order")),
+                TestArguments.of(" order", OrderField(orderDirection = "order"))
+        ).stream()
+    }
+
+    @ParameterizedTest(name = "[{1}->{0}]")
+    @MethodSource("getArguments")
+    fun `toString concatenates id and direction`(expected: String, orderField: OrderField) {
+        assertEquals(expected, orderField.toString())
     }
 }
