779a58a7ba0fb2b6df77b1bc60bb9a01c526406d
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2019-09-10 17:56:07 +0200

    Use parameterized tests

--- src/test/kotlin/it/intre/code/database/reader/filter/generic/ParseHandlerTest.kt
+++ src/test/kotlin/it/intre/code/database/reader/filter/generic/ParseHandlerTest.kt
@@ -1,24 +1,38 @@
 package it.intre.code.database.reader.filter.generic
 
-import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.Test
+import it.intre.code.database.reader.TestArguments
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 class ParseHandlerTest {
-    @Test
-    fun testToBoolean() {
-        assertTrue(toBoolean("true"))
-        assertTrue(toBoolean("TRUE"))
-        assertTrue(toBoolean("True", "tRuE", "TRue"))
-        assertFalse(toBoolean("false"))
-        assertFalse(toBoolean("FALSE", "true"))
-        assertFalse(toBoolean("a", "c;", "DXS"))
-        assertFalse(toBoolean())
-        assertFalse(toBoolean(null, "true"))
+
+    companion object {
+        @JvmStatic
+        fun getArguments(): Stream<Arguments> = listOf<Arguments>(
+                TestArguments.of(true, listOf("true")),
+                TestArguments.of(true, listOf("TRUE")),
+                TestArguments.of(true, listOf("True", "tRuE", "TRue")),
+                TestArguments.of(false, listOf("false")),
+                TestArguments.of(false, listOf("FALSE", "true")),
+                TestArguments.of(false, listOf("a", "c;", "DXS")),
+                TestArguments.of(false, emptyList<String>()),
+                TestArguments.of(false, listOf(null, "true"))
+        ).stream()
     }
+
+    @ParameterizedTest
+    @MethodSource("getArguments")
+    fun `Returns true only if each element is "true" ✅`(expected: Boolean, list: List<String>) {
+        Assertions.assertEquals(expected, ParseHandler.toBoolean(list))
+    }
+
 
     private fun toBoolean(vararg s: String?): Boolean {
         return ParseHandler.toBoolean(listOf(*s))
     }
+
 
 }
