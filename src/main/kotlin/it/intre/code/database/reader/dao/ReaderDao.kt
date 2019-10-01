4f8b85a84bf0271ad24a08474551ab93d7e5d54f
Ranica Marco <ranica@intre.it>
2019-10-01 17:35:20 +0200

    Add ReaderDao

---
+++ src/main/kotlin/it/intre/code/database/reader/dao/ReaderDao.kt
@@ -0,0 +1,14 @@
+package it.intre.code.database.reader.dao
+
+import it.intre.code.database.reader.filter.FilterContainer
+import it.intre.code.database.reader.resultset.ReaderResultSet
+
+/**
+ * Interface for a generic Data Access Object
+ */
+interface ReaderDao {
+
+    fun find(filter: FilterContainer): ReaderResultSet
+
+    fun getColumns(filter: FilterContainer): ReaderResultSet
+}
