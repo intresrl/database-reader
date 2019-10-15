46549a906899d1c438804303dc9b7ed64a56c633
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2019-10-15 17:28:18 +0200

    GenericFilter needs hashCode() and equals()

--- src/main/kotlin/it/intre/code/database/reader/filter/generic/GenericFilter.kt
+++ src/main/kotlin/it/intre/code/database/reader/filter/generic/GenericFilter.kt
@@ -22,4 +22,22 @@ abstract class GenericFilter(
      * @return true only if this filter is creating a meaningful condition
      */
     abstract fun hasCondition(): Boolean
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is GenericFilter) return false
+
+        if (name != other.name) return false
+        if (all != other.all) return false
+        if (negate != other.negate) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + all.hashCode()
+        result = 31 * result + negate.hashCode()
+        return result
+    }
 }
